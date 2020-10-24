a24f14e715e8df4fce70c8b66ffead9e166bed8b
jsmith1309 <jsmith130940@gmail.com>
2020-10-24 12:01:28 +0530

    Updated RockPaperScissors.py
    made some cosmetic changes and rearranged the code a bit.

--- rock paper scissors.py
+++ rock paper scissors.py
@@ -1,24 +1,43 @@
 import random
-global x
-global y
-global com_score
-global player_score
 x = ""
 y = ""
 com_score = 0
 player_score = 0
+game_num = 0
 
-def results():
-	print("Computer picked ", x,"and You picked",  y)
-	print()
+def results(x, y):
+	global com_score, player_score
+	result = ""
+	if x == y:
+		result = "It's a Draw."
+		com_score += 0.5
+		player_score += 0.5
+	elif x == "rock" and y == "paper":
+		result = "You win!"
+		player_score +=1
+	elif x == "rock" and y == "scissors":
+		result = "Computer wins!"
+		com_score +=1
+	elif x == "paper" and y == "rock":
+		result = "Computer wins!"
+		com_score += 1
+	elif x == "paper" and y == "scissors":
+		result = "You win!"
+		player_score +=1
+	elif x == "scissors" and y == "rock":
+		result = "You win!"
+		player_score += 1
+	elif x == "scissors" and y == "paper":
+		result = "Computer wins!"
+		com_score +=1
+	print("Computer picked", x,"and You picked",  y, ".", result, "\n")
+	print("---------------------------------------------------------")
 
 def play ():
-	global x
-	global y
-	global com_score
-	global player_score
+	global x, y, com_score, player_score
+
 	for i in range(5):
-		player = input("enter your choice: \n 1 for rock \n 2 for paper \n 3 for scissors\n")
+		player = input("\nGame number: ( "+ str(i+1) + " ). Score: You(" +str(player_score)+ "), Com(" +str(com_score)+ ") \nEnter your choice: \n (1) for rock \n (2) for paper \n (3) for scissors\n\n    ")
 	
 		if player == "1":
 			y = ("rock")
@@ -42,62 +61,29 @@ def play ():
 		elif computer == 3:
 			x = ("scissors")
 
-		if x  == y:
-			results()
-			print("Draw, both You and Computer are winners")
-			com_score += 1
-			player_score += 1
-		elif x == "rock" and y == "paper":
-			results()
-			print("You are the winner")
-			player_score +=1
-			print()
-		elif x == "rock" and y == "scissors":
-			results()
-			print("Computer is winner")
-			com_score +=1
-			print()
-		elif x == "paper" and y == "rock":
-			results()
-			print("Computer is winner")
-			com_score += 1
-			print()
-		elif x == "paper" and y == "scissors":
-			results()
-			print("You are the winner")
-			player_score +=1
-			print()
-		elif x == "scissors" and y == "rock":
-			results()
-			print("You are the winner")
-			player_score += 1
-			print()
-		elif x == "scissors" and y == "paper":
-			results()
-			print("Computer is the winner")
-			com_score +=1
-			print()
-	print("Final Score: ", "Computer's Score: ",  com_score, "and Your's Score: ", player_score)
+		results(x, y)
 
+name = input("\nEnter your name: ").title()
+print("\nHello,", name, "welcome to the 'Rock-Paper-Scissors' Game")
+print("---------------------------------------------------------")
 
+start = input("Do you want to play?(Y/N): ").upper()
 
-
-
-
-print("Enter your name: ")
-name = input().title()
-print("hello,", name,"welcome to the 'Rock-Paper-Scissors' Game")
-print("------------------------------------------------------")
-print("Do you want to play yes or no")
-start = input().upper()
-
-if start == "YES":
+if start == "Y":
 	print("You have total five chances")
 	print()
 	
 	play()
-else:
+elif start == "N":
 	print("OK, come some other time to play this game, You COWARD!")
+else:
+	print("I did not understand that. Try again later.")
 
-
-
+print("\nFinal Score:")
+print(name,":", player_score, " Computer:",  com_score)
+if com_score > player_score:
+	print("Computer is the WINNER!!")
+elif player_score > com_score:
+	print(name, "is the WINNER!!")
+else:
+	print("It's a draw!")
